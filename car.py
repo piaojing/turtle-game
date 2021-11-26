c1acdd3f2db7da561efc08119c8ae8afc55ac24d
piaojing <vvv198869@gmail.com>
2021-11-26 15:40:03 +0700

    Turtle Car

--- car.py
+++ car.py
@@ -1,17 +1,18 @@
 from turtle import Turtle
+import random
 
 colors = ["red", "black", "blue", "green", "purple"]
 
 
 class Car(Turtle):
-    def __init__(self) -> None:
+    def __init__(self, y_pos) -> None:
         super().__init__()
         self.hideturtle()
         self.shape("square")
         self.penup()
-        self.color("black")
-        self.shapesize(1, 3)
-        self.goto(-400, 0)
+        self.color(random.choice(colors))
+        self.shapesize(1, random.randrange(3, 5, 1))
+        self.goto(random.randrange(-420, -800, -50), y_pos)
         self.showturtle()
 
     def move(self):
--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from turtle import Screen
 from my_turtule import MyTurtle
 import time
 from car import Car
+import random
+
 
 my_turtle = MyTurtle()
-car = Car()
+
+# make car
+rand_y = random.sample(range(-5, 5), 8)
+print("Rand Y Position: ", rand_y)
+car_list = []
+for car in range(8):
+    car = Car(rand_y[car] * 40)
+    car_list.append(car)
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -15,10 +24,15 @@ screen.onkey(my_turtle.go_down, "Down")
 
 
 screen.listen()
-
+new_car = Car(random.randint(-250, 250))
 game_is_on = True
 while game_is_on:
-    car.move()
+    # for i in range(8):
+    #     car_list[i].move()
+    new_car.move()
+    if new_car.xcor == -350:
+        new_car = Car(random.randint(-250, 250))
+
     time.sleep(0.1)
     screen.update()  # Perform a TurtleScreen update. To be used when tracer is turned off.
 
